186f14eec8c87648f9788287411646f7251291a9
Virak-Tav <viraktav@gmail.com>
2017-06-30 08:30:06 +0700

    commit header and footer

--- assan/src/index.js
+++ assan/src/index.js
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
 
-const root = document.getElementById('root');
-
-ReactDOM.render(
-    (<BrowserRouter><App /></BrowserRouter>),
-    root
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
